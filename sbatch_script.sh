17da85be76cd724263756d1817e958205cc657b1
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2025-03-20 00:03:37 +0800

    update gpu requirement

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=BNI                   # Job name
 #SBATCH --time=48:00:00                  # Time limit hrs:min:sec
-#SBATCH --gres=gpu:nv:1
+#SBATCH --gres=gpu:a100-40:1
 #SBATCH --mail-type=ALL                  # Get email for all status updates
 #SBATCH --mail-user=e0407638@u.nus.edu   # Email for notifications
 #SBATCH --mem=16G                        # Request 16GB of memory
